fdb1a016c6d6c86e7951a54114b4f40d16a43f7b
Renzo <renzocallachavez@gmail.com>
2024-03-20 20:04:52 -0400

    addin sss styles with scss

---
+++ src/components/Tester/Tester.tsx
@@ -0,0 +1,10 @@
+import classes from "./Tester.module.scss";
+const Tester = () => {
+  return (
+    <p className={classes.paragraph}>
+      this is some component to test the scss modules
+    </p>
+  );
+};
+
+export default Tester;
