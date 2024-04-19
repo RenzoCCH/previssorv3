da386f0eaa5c6c5725e528acb0ef1b478efd3838
Renzo Calla <renzo.calla@sehpora.com>
2024-04-19 19:29:27 -0400

    commit

--- src/services/quizService.ts
+++ src/services/quizService.ts
@@ -7,9 +7,32 @@ interface ResponseError extends Error {
   status?: number;
 }
 
+// export const getQuiz = async (quizToken: string): Promise<QuizTaken> => {
+//   try {
+//     const url = import.meta.env.VITE_API + quizToken;
+//     const response = await fetch(url);
+
+//     if (!response.ok) {
+//       const err: ResponseError = new Error(response.statusText);
+//       err.status = response.status;
+//       throw err;
+//     }
+
+//     const data = await response.json();
+//     return data.quiz;
+//   } catch (e) {
+//     let message = i18next.t("error.no_server");
+//     if ((e as ResponseError).status === 404) {
+//       message = i18next.t("error.not_found");
+//     }
+//     throw new Error(message);
+//   }
+// };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getQuiz = async (quizToken: string): Promise<QuizTaken> => {
   try {
-    const url = import.meta.env.VITE_API + quizToken;
+    const url = import.meta.env.VITE_GRAPHQL;
     const response = await fetch(url);
 
     if (!response.ok) {
